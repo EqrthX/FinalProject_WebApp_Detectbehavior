009b65377477c6c2cbf3092db110b118890b9a6f
EqrthX <nontprawitchkung@gmail.com>
2025-12-23 21:23:26 +0700

    refactor: changes path model

--- server/utils/model_loader.py
+++ server/utils/model_loader.py
@@ -11,7 +11,7 @@ def get_model_path():
     MODEL_PATH = os.path.abspath(os.path.join(
         BASE_DIR,
         "..", "..",
-        "datasetv6.1", "datasetv6.1", "detect", "train", "weights", "best.pt"
+        "dataset_test", "project", "train", "weights", "best.pt"
     ))
 
     return MODEL_PATH
